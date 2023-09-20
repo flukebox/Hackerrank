040ea5989a5722699440e383a329abef99cccbbe
flukebox <jaikumar.iitk@gmail.com>
2023-09-20 13:51:34 +0530

    Started SQL practice

---
+++ hack-interview-prep/sql/scratch.sql
@@ -0,0 +1,262 @@
+--Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+--The CITY table is described as follows:
+--
+--FIELD  TYPE
+--ID     NUMBER
+--NAME   VARCHAR2(17)
+--COUNTRYCODE VARCHAR2(3)
+--DISTRICT VARCHAR2(20)
+--POPULATION NUMBER
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN'
+
+
+--Query a list of CITY and STATE from the STATION table.
+
+SELECT DISTINCT CITY, STATE FROM STATION;
+
+--Query a list of CITY names from STATION for cities that have an even ID number.
+-- Print the results in any order, but exclude duplicates from the answer.
+SELECT DISTINCT CITY FROM STATION WHERE MOD(ID,2)=0 ORDER BY CITY ASC;
+
+
+--Find the difference between the total number of CITY entries in the table and the number
+-- of distinct CITY entries in the table.
+--For example, if there are three records in the table with CITY values 'New York', 'New York', 'Bengalaru',
+-- there are 2 different city names: 'New York' and 'Bengalaru'. The query returns , because .
+
+SELECT COUNT(CITY)-COUNT(DISTINCT CITY) FROM STATION;
+
+
+--Query the two cities in STATION with the shortest and longest CITY names, as well as
+--their respective lengths (i.e.: number of characters in the name). If there is more
+--than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+--Sample Input
+--
+--For example, CITY has four entries: DEF, ABC, PQRS and WXY.
+--
+--Sample Output
+--
+--ABC 3
+--PQRS 4
+--Explanation
+--
+--When ordered alphabetically, the CITY names are listed as ABC, DEF, PQRS, and WXY, with lengths
+-- and . The longest name is PQRS, but there are  options for shortest named city. Choose ABC,
+-- because it comes first alphabetically.
+--
+--Note
+--You can write two separate queries to get the desired output. It need not be a single query.
+
+select city, length(city) from station order by length(city) DESC,city ASC fetch first row only;
+select city, length(city) from station order by length(city) asc ,city asc fetch first row only;
+
+
+--Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION.
+--Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION WHERE SUBSTRING(CITY,1,1) in ('A', 'E', 'I', 'O', 'U') ORDER BY CITY ASC
+SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%';
+
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u';
+
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION WHERE (CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%') AND  (CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u)';
+
+
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE SUBSTRING(CITY,1,1) not in ('A', 'E', 'I', 'O', 'U') ORDER BY CITY ASC
+
+--Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY,1) not in ('a', 'e', 'i', 'o', 'u') ORDER BY CITY ASC
+
+--Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE SUBSTRING(CITY,1,1) not in ('A', 'E', 'I', 'O', 'U') OR RIGHT(CITY,1) not in ('a', 'e', 'i', 'o', 'u')  ORDER BY CITY ASC
+
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE SUBSTRING(CITY,1,1) not in ('A', 'E', 'I', 'O', 'U') AND RIGHT(CITY,1) not in ('a', 'e', 'i', 'o', 'u')  ORDER BY CITY ASC
+
+
+--Write a query identifying the type of each record in the TRIANGLES table using its three side lengths.
+--Output one of the following statements for each record in the table:
+--
+--Equilateral: It's a triangle with 3 sides of equal length.
+--Isosceles: It's a triangle with 2 sides of equal length.
+--Scalene: It's a triangle with 3 sides of differing lengths.
+--Not A Triangle: The given values of A, B, and C don't form a triangle.
+
+SELECT
+CASE
+WHEN (A+B <= C OR B+C <= A OR A+C <= B) THEN 'Not A Triangle'
+WHEN (A=B and B=C) THEN 'Equilateral'
+WHEN (A=B OR B=C OR C=A) THEN 'Isosceles'
+ELSE 'Scalene'
+END
+FROM TRIANGLES
+
+--Generate the following two result sets:
+--Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each
+--profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D),
+--AProfessorName(P), and ASingerName(S).
+--Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output
+--them in the following format:
+--
+--There are a total of [occupation_count] [occupation]s.
+--where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is
+--the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be
+--ordered alphabetically.
+--
+--Note: There will be at least two entries in the table for each type of occupation.
+--
+--Input Format
+--
+--The OCCUPATIONS table is described as follows:
+--FIELD  TYPE
+--NAME   VARCHAR2
+--OCCUPATION   VARCHAR2
+
+--Jane(A)
+--Jenny(D)
+--Julia(A)
+--Ketty(P)
+--Maria(A)
+--Meera(S)
+--Priya(S)
+--Samantha(D)
+--There are a total of 2 doctors.
+--There are a total of 2 singers.
+--There are a total of 3 actors.
+--There are a total of 3 professors.
+--
+
+SELECT CONCAT(NAME, "(", LEFT(OCCUPATION, 1), ")") FROM OCCUPATIONS ORDER BY NAME ASC;
+SELECT CONCAT("There are a total of ", COUNT(OCCUPATION)," ", Lower(OCCUPATION),"s.") FROM OCCUPATIONS GROUP by OCCUPATION ORDER BY COUNT(OCCUPATION) asc, LEFT(OCCUPATION, 1) asc;
+
+--Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and
+--displayed underneath its corresponding Occupation. The output column headers should be Doctor, Professor, Singer,
+--and Actor, respectively.
+--
+--Note: Print NULL when there are no more names corresponding to an occupation.
+
+select
+    -- Get Names for Occupations for each RowOrder
+    D, P, S, A
+from (
+    -- Row of RN, Occupation Group by RN for each Name
+    select
+        RN,
+        max(case Occupation when 'Doctor' then Name end) as D,
+        max(case Occupation when 'Professor' then Name end) as P,
+        max(case Occupation when 'Singer' then Name end) as S,
+        max(case Occupation when 'Actor' then Name end) as A
+    from (
+    -- Order Occupation, Name, RowNumber (of Name within List of same Occupation ord by Name asc)
+            select
+                Occupation, Name, row_number() over(partition by Occupation order by Name ASC) as RN
+            from Occupations
+         ) as NameWithRN
+    group by RN
+    ) as JustNames
+
+--
+--You are given a table, BST, containing two columns: N and P, where N represents the value of a node in Binary Tree, and P is the parent of N.
+--
+--
+--
+--Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
+--
+--Root: If node is root node.
+--Leaf: If node is leaf node.
+--Inner: If node is neither root nor leaf node.
+--Sample Input
+--
+--
+--
+--Sample Output
+--
+--1 Leaf
+--2 Inner
+--3 Leaf
+--5 Root
+--6 Leaf
+--8 Inner
+--9 Leaf
+--
+--Explanation
+--
+--The Binary Tree below illustrates the sample:
+--
+--
+--
+SELECT BST.N, CASE
+WHEN (BST.P is not  NULL and  MAX(BST1.N) is not NULL) then  "Inner"
+WHEN (BST.P is NULL) then  "Root"
+WHEN (MAX(BST1.N)  is NULL) then  "Leaf"
+END
+from BST left join (SELECT N, P from BST) as BST1 on BST1.P = BST.N
+GROUP BY BST.N, BST.P
+ORDER BY BST.N;
+
+
+--Amber's conglomerate corporation just acquired some new companies. Each of the companies follows this hierarchy:
+--Given the table schemas below, write a query to print the company_code, founder name, total number of lead managers,
+--total number of senior managers, total number of managers, and total number of employees. Order your output by
+--ascending company_code.
+
+--select company_code, founder from Company;
+--
+--
+--select lead_manager_code, company_code from Lead_Manager;
+--select senior_manager_code, lead_manager_code, company_code from Senior_Manager;
+--select manager_code, senior_manager_code, lead_manager_code, company_code from Manager;
+--select employee_code, manager_code, senior_manager_code, lead_manager_code, company_code from Employee;
+--
+
+select company_code, founder from Company;
+select lead_manager_code, company_code from Lead_Manager;
+select senior_manager_code, lead_manager_code, company_code from Senior_Manager;
+select manager_code, senior_manager_code, lead_manager_code, company_code from Manager;
+
+select  J.company_code,  J.founder,
+count(distinct(J.lead_manager_code)), count(distinct(J.senior_manager_code)),
+count(distinct(J.manager_code)), count(distinct(J.employee_code))
+from (
+select C.company_code,  C.founder, employee_code, manager_code, senior_manager_code, lead_manager_code
+from Employee as E join Company as C on C.company_code = E.company_code) As J
+group by J.company_code, J.founder;
+
+
+
+
+--A median is defined as a number separating the higher half of a data set from the lower half.
+--Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to  decimal places.
+with ranked as (
+  select LAT_N,
+    row_number() over (order by LAT_N) as r,
+    count(LAT_N) over () as c
+  from STATION
+),
+median as (
+  select LAT_N
+  from ranked
+  where r in (floor((c+1)/2), ceil((c+1)/2))
+)
+select Round(avg(LAT_N), 4) from median
+
+--Query the average population of all cities in CITY where District is California.
+--
+--Input Format
+--
+--The CITY table is described as follows:
+--
+--FIELD  TYPE
+--ID     NUMBER
+--NAME   VARCHAR2(17)
+--COUNTRYCODE VARCHAR2(3)
+--DISTRICT VARCHAR2(20)
+--POPULATION NUMBER
+
+select avg(POPULATION) from CITY where DISTRICT = 'California'
+

f1305c91e8df12165c7e74fdd184e957739b2823
flukebox <jaikumar.iitk@gmail.com>
2023-09-21 22:03:22 +0530

    SQL 5 star badges

--- hack-interview-prep/sql/scratch.sql
+++ hack-interview-prep/sql/scratch.sql
@@ -257,6 +257,150 @@ select Round(avg(LAT_N), 4) from median
 --COUNTRYCODE VARCHAR2(3)
 --DISTRICT VARCHAR2(20)
 --POPULATION NUMBER
-
 select avg(POPULATION) from CITY where DISTRICT = 'California'
+
+
+--Query the average population for all cities in CITY, rounded down to the nearest integer.
+select Round(avg(POPULATION), 0) from CITY;
+
+
+--Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table,
+--but did not realize her keyboard's  key was broken until after completing the calculation. She wants your
+--help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual
+--average salary.
+--
+--Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer.
+--
+--Input Format
+--
+--The EMPLOYEES table is described as follows:
+--
+--FIELD  TYPE
+--ID     NUMBER
+--NAME   VARCHAR2(17)
+--SALARY  NUMBER
+
+SELECT CEIL(AVG(SALARY)-AVG(CAST(REPLACE(CONCAT(SALARY), "0", "") as UNSIGNED))) from EMPLOYEES;
+
+
+--We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to
+--be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total
+--earnings for all employees as well as the total number of employees who have maximum total earnings.
+--Then print these values as  space-separated integers.
+-- The Employee table containing employee data for a company is described as follows:
+
+--Input Format
+--
+--The EMPLOYEE table is described as follows:
+--
+--EMPLOYEE_ID     NUMBER
+--NAME   VARCHAR2(17)
+--SALARY  NUMBER
+--MONTHS  NUMBER
+
+SELECT SALARY*MONTHS as EARNING,COUNT(*)
+FROM EMPLOYEE
+GROUP BY EARNING
+ORDER BY EARNING DESC LIMIT 1
+
+
+-- Weather Observation Station 2
+--Query the following two values from the STATION table:
+--
+--The sum of all values in LAT_N rounded to a scale of  decimal places.
+--The sum of all values in LONG_W rounded to a scale of  decimal places.
+--Input Format
+--
+--The STATION table is described as follows:
+
+SELECT ROUND(SUM(LAT_N), 2), ROUND(SUM(LONG_W), 2)  FROM STATION;
+
+
+--Weather Observation Station 13
+--
+--Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880  and less 137.2345 than .
+--Truncate your answer to 4 decimal places.
+
+
+SELECT ROUND(SUM(LAT_N), 4) FROM STATION
+WHERE LAT_N > 38.7880 AND LAT_N <  137.2345;
+
+
+--Weather Observation Station 14
+--
+--Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345.
+-- Truncate your answer to 4  decimal places.
+
+SELECT ROUND(MAX(LAT_N), 4) FROM STATION
+WHERE LAT_N <  137.2345;
+
+--Weather Observation Station 15
+--
+--Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345.
+-- Truncate your answer to 4  decimal places.
+
+SELECT ROUND(LONG_W, 4) FROM STATION AS S1 JOIN
+(SELECT MAX(LAT_N) AS LAT_N FROM STATION WHERE LAT_N <  137.2345) AS S2 ON S1.LAT_N = S2.LAT_N
+LIMIT 1;
+
+
+
+--Weather Observation Station 16
+--
+-- Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780.
+-- Truncate your answer to 4  decimal places.
+SELECT ROUND(MIN(LAT_N), 4) FROM STATION WHERE LAT_N >  38.7780
+LIMIT 1;
+
+--Weather Observation Station 17
+--Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780.
+-- Truncate your answer to 4  decimal places.
+SELECT ROUND(LONG_W, 4) FROM STATION AS S1 JOIN
+(SELECT MIN(LAT_N) AS LAT_N FROM STATION WHERE LAT_N >  38.7780) AS S2 ON S1.LAT_N = S2.LAT_N
+LIMIT 1;
+
+
+--Weather Observation Station 18
+--Consider P1(a,b) and P2(c, d) to be two points on a 2D plane.
+--
+--a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+--b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+--c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+--d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+--Query the Manhattan Distance between points  P1 and P2 and round it to a scale of 4 decimal places.
+
+--Definition: The distance between two points measured along axes at right angles.
+--In a plane with p1 at (x1, y1) and p2 at (x2, y2), it is |x1 - x2| + |y1 - y2|.
+SELECT ROUND((MAX(LAT_N) - MIN(LAT_N)) + (MAX(LONG_W) - MIN(LONG_W)), 4) FROM STATION;
+
+--Weather Observation Station 19
+--Consider P1(a,b) and P2(c, d) to be two points on a 2D plane.
+--
+--a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+--b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+--c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+--d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+--Query the Euclidean Distance between points  P1 and P2 and round it to a scale of 4 decimal places.
+SELECT ROUND(SQRT((X*X+Y*Y)), 4) FROM(
+SELECT (MAX(LAT_N)-MIN(LAT_N)) as X, (MAX(LONG_W)-MIN(LONG_W)) as Y FROM STATION) AS XY;
+
+
+--African Cities
+--Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+--
+--Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+--
+SELECT CITY.NAME FROM CITY
+JOIN (SELECT * FROM COUNTRY WHERE CONTINENT = 'AFRICA') AS COUNTRY
+ON CITY.CountryCode = COUNTRY.Code;
+
+--Average Population of Each Continent
+--Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent)
+--and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+--
+--Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+SELECT COUNTRY.CONTINENT, FLOOR(AVG(CITY.POPULATION)) FROM CITY
+JOIN COUNTRY
+ON CITY.CountryCode = COUNTRY.Code
+GROUP BY COUNTRY.CONTINENT;
 
